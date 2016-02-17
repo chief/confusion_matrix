43e51cb81a4b308ddf2176864d1c25077738a18f
Giorgos Tsiftsis <giorgos.tsiftsis@skroutz.gr>
2016-02-17 13:52:43 +0200

    Update project desc and url

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject confusion_matrix "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Metrics for classification algorithms performance."
+  :url "https://github.com/skroutz/confusion_matrix"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
